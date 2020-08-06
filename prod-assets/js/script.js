903fdf7c0d6f3d076b6feff0472ddb86be6171ce
unknown <szymon.piatkowski27@gmail.com>
2020-08-06 15:41:50 +0200

    Script for scroll effects
    I added jQuery scrollTop method which help to achive animation
    on define position, also added script for scrollTop button
    to scroll up page when we click on the button and also added read more
    button through adding new classes to existing element.

--- prod-assets/js/script.js
+++ prod-assets/js/script.js
@@ -1,5 +1,5 @@
 // ITEMS FROM DOM
-const navButton = document.getElementById('nav-button');
+let navButton = document.getElementById('nav-button');
 
 
 // HAMBURGER MENU FUNCTION
@@ -14,20 +14,57 @@ addClassChange = () => {
 },50);
 
 // SCROLL TOP APPEAR BACKGROUND ON NAVBAR
-$(window).scroll(function(){
+$(window).scroll(()=>{
   let position = $(this).scrollTop();
-  if(position >=800){
+  if(position >=650){
     $('.header-transparent').addClass('color');
 
 
   }else {
     $('.header-transparent').removeClass('color');
 
-
-
-
   }
 });
+// Scroll to top button
+const scrollToTop = document.querySelector("#scrollToTop");
+scrollToTop.addEventListener('click', function (){
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior: 'smooth'
+  });
+});
+
+// SCROLL ANIMATION ON ABOUT US ITEMS
+$(window).scroll(()=>{
+  let position = $(this).scrollTop();
+  if(position >=50){
+    $('.about-us').addClass('about-us__left');
+    $('.about-us2').addClass('about-us__right');
+  }
+  else {
+    $('.about-us').removeClass('about-us__left');
+    $('.about-us2').removeClass('about-us__right');
+  }
+  if(position >= 750){
+    $('.about-us3').addClass('about-us__left');
+    $('.about-us4').addClass('about-us__right');
+  }else {
+    $('.about-us3').removeClass('about-us__left');
+    $('.about-us4').removeClass('about-us__right');
+  }
+});
+// Read more or read less button
+document.querySelectorAll(".showmore").forEach(function (p) {
+  p.querySelector("a").addEventListener("click", function () {
+    p.classList.toggle("show");
+    console.log(this);
+    this.textContent = p.classList.contains("show") ? "Read Less" : "Read More";
+  });
+});
+
+
+
 
 
 
